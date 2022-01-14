3bc641a492db234d9e490f61f0aadae900fc1763
figletechnologies <support@figletechnologies.com>
2022-01-14 19:57:01 +0530

    Your comment

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const app = express()
 const mongoose = require('mongoose')
 
 //mongoose.connect('mongodb://127.0.0.1:27017/wikiDB')
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://foodappdebug:foodappdebug@cluster0.avnbx.mongodb.net/foodappdebug?retryWrites=true&w=majority')
 
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -21,7 +21,8 @@ app.use('/users', userrouter)
 
 //'localhost:3000/articles/sdfsdf'
 
-
+const vendorsrouter = require('./routes/vendors')
+app.use('/vendors', vendorsrouter)
 
 
 
